768c1b6b8d58fac7f51a32148d9265a2b169589d
HouYaLong <554121780@qq.com>
2021-07-06 18:48:48 +0800

    前期框架设计

---
+++ editer.js
@@ -0,0 +1,16 @@
+// 新增
+  // 新增文本
+  createTxt
+  // 新增图片
+  createImage
+  // 新增基本图形
+// 移除
+delItem
+// 更新样式：坐标 宽高 尺寸 旋转 翻转 锁定等 
+setCss
+// 更新可见度
+toggleVisiable
+// 激活当前编辑对象
+setActiveSelect
+//更新层级
+updateLevel
